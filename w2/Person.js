9198667254d79ce87f4a594cf1a062785dc69091
Shri Khare <khare@exploremetakinetic.com>
2021-11-18 22:44:59 -0500

    refactored assertequal and head

--- w2/Person.js
+++ w2/Person.js
@@ -1,11 +1,14 @@
 class Person {
-    constructor(name, quirkyFact) {
+    constructor(name, quirkyFact, location) {
         this.name = name;
         this.quirkyFact = quirkyFact;
-    
+        this.location = location;
     }
     bio() {
         return `My name is ${this.name} and here's my quirky fact: ${this.quirkyFact}`;
+    }
+    city() {
+        return `I am currently in ${this.location}`
     }
 }
 
@@ -13,6 +16,9 @@ class Student extends Person {
 
     enroll(cohort) {
         this.cohort = cohort;
+    }
+    bio() {
+        return `I'm a student at Lighthouse Labs (aka Labber). ${super.bio()}`
     }
 }
 class Instructor extends Person {
@@ -23,8 +29,14 @@ class Instructor extends Person {
         this.onShift = false; 
      }
 }
-
-// var student = new Student('Backend Python');
-// student = new Person('Shri', 'I like pineapple on pizza');
-// // student.enroll('Backend Python');
-// console.log(student)
+let student = new Student('Shri','I like pineapple on pizza', 'Kingston');
+let myInstructor = new Instructor('John', 'I like to sing on the subway', 'Ottawa');
+student.enroll('Backend Python');
+myInstructor.goOnShift();
+myInstructor.goOffShift();
+console.log(student);
+console.log(student.bio());
+console.log(student.city());
+console.log(myInstructor);
+console.log(myInstructor.bio());
+console.log(myInstructor.city());
--- w2/Pizza.js
+++ w2/Pizza.js
@@ -4,19 +4,37 @@ class Pizza {
         this.crust = crust;
         this.toppings = ['Cheese'];
     }
-
-    addTopping(topping){
+    get price() {
+        const basePrice = 10;
+        const toppingPrice = 2;
+        return basePrice + (this.toppings.length * toppingPrice);
+    }
+    addTopping(topping) {
         this.toppings.push(topping);
     }
+    set size(size) {
+        if(size === 's' || size === 'm' || size === 'l'){
+            this._size = size;
+        }
+    }
+    getSize(size) {
+        return this.size;
+    }
 }
+//DRIVER CODE
+
 let pizza = new Pizza('large', 'thin');
 // let pizza1 = new Pizza();
 // console.log(pizza1.toppings);
 // pizza1.addTopping("mushroom");
 // pizza1.addTopping("peppers");
 // console.log(pizza1.toppings);
-console.log(pizza);
 // let pizza2 = new Pizza();
 // console.log(pizza2.toppings);
 // pizza2.addTopping('more cheese');
 // console.log(pizza2.toppings);
+let pizza2 = new Pizza();
+pizza2.size = 's';
+pizza2.getSize();
+pizza2.price;
+console.log(pizza2.price);
